23bed47d404106e290349928d27e81f901f3d27b
Naomi Higareda <nfhigareda@actx.edu>
2025-02-18 13:00:18 -0600

    Add project files.

---
+++ WK6Quiz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WK6Quiz
+{
+    internal class Program
+    {
+        public static string ReverseString(string input)
+            {
+            //create method for reverse string operation
+            string ReverseString = input;
+            return input;
+            }
+        public static string CountVowels(string input)
+        {
+            //create method for counting vowels
+            string CountVowels = input;
+            return input;
+        }
+        public static string IsPalindrome(string input)
+        {
+            string palindrome = input;
+            return IsPalindrome(input);
+        }
+        static void Main(string[] args)
+        {
+            //prompt user to input a string
+            Console.WriteLine("Input a string");
+            string input = Console.ReadLine();
+        }    
+    }
+}
